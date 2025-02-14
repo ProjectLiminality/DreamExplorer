ea86372d54d2666a0ada5314984c81c01177a0f1
Interface Guy <david.rug98@icloud.com>
2025-02-14 16:14:19 +0000

    updated placeholder text

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <h1>Welcome to Dream Explorer</h1>
-      <p>This is a placeholder text for the landing page. Scroll up to see the DreamSpace component.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt nunc, vitae aliquam nunc nunc vitae nunc. Sed euismod, nunc id aliquam tincidunt, nisl nunc tincidunt nunc, vitae aliquam nunc nunc vitae nunc.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt nunc, vitae aliquam nunc nunc vitae nunc. Sed euismod, nunc id aliquam tincidunt, nisl nunc tincidunt nunc, vitae aliquam nunc nunc vitae nunc.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt nunc, vitae aliquam nunc nunc vitae nunc. Sed euismod, nunc id aliquam tincidunt, nisl nunc tincidunt nunc, vitae aliquam nunc nunc vitae nunc.</p>
+      <h1>Home of Project Liminality</h1>
+      <p>Transmuting childhood wounds of social fragmentation into memetic medicine for the human family</p>
+      <p>Developing a novel communication medium for a new and ancient way of serving stories as beings</p>
+      <p>Helping visionaries deepen their service to humanity through Liminal Consulting</p>
     </div>
   );
 };
